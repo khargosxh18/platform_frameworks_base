499b8d17c6e441ab372a7714088fc83f4eecc9d0
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-20 08:45:49 +0800

    inputmethod: allow hiding navigation ime bar [1/4]
    Change-Id: Iaa15dda08d614d01fae64ae2b5aa72f50513af99
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14051,6 +14051,12 @@ public final class Settings {
         public static final String PI_SNAPCHAT_SPOOF = "pi_snapchat_spoof";
 
         /**
+         * Show navigation space below IME
+         * @hide
+         */
+        public static final String NAVBAR_IME_SPACE = "navbar_ime_space";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
