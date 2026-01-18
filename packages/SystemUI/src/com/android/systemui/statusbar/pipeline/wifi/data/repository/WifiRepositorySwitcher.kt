5a12154c7cbe39938bad9034f87046d6cf7504fd
Pranav Vashi <neobuddy89@gmail.com>
2026-01-19 00:01:23 +0530

    SystemUI: Improve wifi/cell tiles
    * Make the WiFi tile actually disable WiFi instead of "pausing"
    * In the expanded state, when clicking on the secondary action
      change the popup dialog title from "Internet" to "Wi-Fi".
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I9911d675e3c3c6c0eedb0b9e11efdcbae4033e19
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/wifi/data/repository/WifiRepositorySwitcher.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/wifi/data/repository/WifiRepositorySwitcher.kt
@@ -140,6 +140,10 @@ constructor(
             .flatMapLatest { it.wifiToggleState }
             .stateIn(scope, SharingStarted.WhileSubscribed(), realImpl.wifiToggleState.value)
 
+    override fun disableWifi() {
+        activeRepo.value.disableWifi()
+    }
+
     override fun enableWifi() {
         activeRepo.value.enableWifi()
     }
